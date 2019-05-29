9fc91aa2f08c2aaa3cdba69141bc41d1fe280900
Md Nazmul Karim <mdnazmulkarim@u.boisestate.edu>
2019-05-29 14:17:07 -0600

    first coommit

---
+++ render1.js
@@ -0,0 +1,9 @@
+	
+'use restrict';
+
+const domContainer = document.querySelector('#like_button_container');	
+ReactDOM.render(<h1>BSU Computer Science</h1>, document.getElementById("root"));
+	
+
+	
+	
